71d270e017d8a4f20fc604e5aee7fdf55faab9ea
Tianyi-Fu <fut4@cardiff.ac.uk>
2023-07-07 07:23:03 +0100

    add openai api, but it's no working for now before getting paid.

--- src/main/java/com/demo/controller/DemoController.java
+++ src/main/java/com/demo/controller/DemoController.java
@@ -8,7 +8,12 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import com.azure.ai.openai.OpenAIClient;
+import com.azure.ai.openai.OpenAIClientBuilder;
+import com.azure.ai.openai.models.*;
+import com.azure.core.credential.AzureKeyCredential;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -75,5 +80,56 @@ public class DemoController {
         demoService.delete(id);
         return Result.success();
     }
+//    @GetMapping("/azure-openai-chat")
+//    public static String azureOpenaiChat() {
+//        OpenAIClient client = new OpenAIClientBuilder()
+//                // Azure OpenAI key
+//                .credential(new AzureKeyCredential(""))
+//                .endpoint("")
+//                .buildClient();
+//        List<ChatMessage> chatMessages = new ArrayList<>();
+//        chatMessages.add(new ChatMessage(ChatRole.SYSTEM).setContent("what's your name"));
+//        chatMessages.add(new ChatMessage(ChatRole.USER).setContent("what's your mom"));
+//        chatMessages.add(new ChatMessage(ChatRole.ASSISTANT).setContent("Sing something"));
+//        chatMessages.add(new ChatMessage(ChatRole.USER).setContent("are you happy?"));
+//        ChatCompletions chatCompletions = client.getChatCompletions("ppt-gpt", new ChatCompletionsOptions(chatMessages));
+//        StringBuilder sb = new StringBuilder();
+//        for (ChatChoice choice : chatCompletions.getChoices()) {
+//            ChatMessage message = choice.getMessage();
+//            sb.append(message.getContent());
+//        }
+//        return sb.toString();
+//    }
+
+    @GetMapping("/openai-chat")
+    public static String openaiChat() {
+        OpenAIClient client = new OpenAIClientBuilder()
+                // OpenAI key
+                .credential(new NonAzureOpenAIKeyCredential("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"))
+                .buildClient();
+
+        // message
+        List<ChatMessage> chatMessages = new ArrayList<>();
+        chatMessages.add(new ChatMessage(ChatRole.SYSTEM).setContent("what's your name"));
+        chatMessages.add(new ChatMessage(ChatRole.USER).setContent("what's your mom"));
+        chatMessages.add(new ChatMessage(ChatRole.ASSISTANT).setContent("Sing something"));
+        chatMessages.add(new ChatMessage(ChatRole.USER).setContent("are you happy?"));
+        ChatCompletions chatCompletions = client.getChatCompletions("gpt-3.5-turbo-16k", new ChatCompletionsOptions(chatMessages));
+
+        StringBuilder sb = new StringBuilder();
+        for (ChatChoice choice : chatCompletions.getChoices()) {
+            ChatMessage message = choice.getMessage();
+            sb.append(message.getContent());
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String openaiChat = openaiChat();
+        //String azureOpenaiChat = azureOpenaiChat();
+        System.out.println("openaiChat: " + openaiChat);
+        //System.out.println("azureOpenaiChat: " + azureOpenaiChat);
+    }
+
 
 }
