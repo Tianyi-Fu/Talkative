4d0fce726a1cd86db05beee9c02e4524839b1253
Po <tangp3@cardiff.ac.uk>
2023-08-14 00:57:15 +0100

    submit cha widget transcript to backend works

--- frontend/js/chatWidget.js
+++ frontend/js/chatWidget.js
@@ -5,19 +5,17 @@ $(document).ready(() => {
         // Track the changes in the chat widget
         let observer = new MutationObserver(function (m) {
             if (shadow.querySelector("button[title='End Interaction']")) {
-                // console.log('found End Interaction')
 
-                // let chatBody = shadow.querySelector("div#app .bg-white.overflow-y-auto.flex.flex-grow")
                 let observer = new MutationObserver((m) => {
                     if (shadow.querySelector("button[title='Finish']")) {
                         console.log('found Finish2')
 
                         // If customer confirm ends conversation, extract conversation transcript
-                        let customer = shadow.querySelectorAll("#main-app .bg-primary .whitespace-pre-line");
-                        customer.forEach((item) => console.log(item.innerText));
-
-                        let agent = shadow.querySelectorAll("#main-app .bg-white .whitespace-pre-line");
-                        agent.forEach((item) => console.log(item.innerText));
+                        // let customer = shadow.querySelectorAll("#main-app .bg-primary .whitespace-pre-line");
+                        // customer.forEach((item) => console.log(item.innerText));
+                        //
+                        // let agent = shadow.querySelectorAll("#main-app .bg-white .whitespace-pre-line");
+                        // agent.forEach((item) => console.log(item.innerText));
 
                         let conversation = shadow.querySelector(".chat-thread-container").childNodes;
                         let conversationArr = Array.from(conversation);
@@ -26,11 +24,44 @@ $(document).ready(() => {
                         let customerName = "flex pt-2 justify-end text-right";
                         let customerMsg = "flex flex-wrap space-y-2 justify-end pl-12";
 
-                        conversationArr.filter((child)=> {
-                            if(child.className === agentName || child.className === agentMsg || child.className === customerName || child.className === customerMsg){
-                                console.log(child.lastElementChild.innerText)
-                            //    TODO: form object for each pair
+                        let conversationObj = [];
+                        let currentPair = {};
+                        let sender_agent;
+
+                        conversationArr.forEach((child) => {
+                            let className = child.className;
+                            let textContent = child.lastElementChild?.innerText || ""; // Get innerText safely
+
+                            if (className === agentName) {
+                                currentPair.agentName = textContent;
+                                sender_agent = textContent;
+                            } else if (className === agentMsg) {
+                                if (!currentPair.agentName) {
+                                    conversationObj.findLastIndex((obj) => currentPair.agentName = obj.agentName)
+                                }
+                                currentPair.agentMsg = textContent;
+                                conversationObj.push(currentPair);
+                                currentPair = {};
+                            } else if (className === customerName) {
+                                currentPair.customerName = textContent;
+                            } else if (className === customerMsg) {
+                                if (!currentPair.customerName) {
+                                    conversationObj.findLastIndex((obj) => currentPair.customerName = obj.customerName)
+                                }
+                                currentPair.customerMsg = textContent;
+                                conversationObj.push(currentPair);
+                                currentPair = {};
                             }
+                        });
+
+                        let newConversation = [];
+                        conversationObj.map((item) => {
+                            newConversation.push({[item.agentName || item.customerName]: item.agentMsg || item.customerMsg});
+                        })
+                        console.log(newConversation);
+
+                        $(shadow.querySelector("button[title='Finish']")).click(() => {
+                            createTranscriptObject({"agent_name": sender_agent, "transcript": newConversation});
                         })
                     }
                 });
@@ -41,9 +72,9 @@ $(document).ready(() => {
         observer.observe(shadow, {childList: true});
     }, 3000)
 
-    function createTranscriptObject() {
+    function createTranscriptObject(transcriptObj) {
         let id = Date.now();
-        let messages = [];
+        sendTranscript(id, {...transcriptObj, "chatRecordId": id}, true);
     }
 
     /*
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,66 +1,22 @@
+let baseUrl = 'http://127.0.0.1:8081'
+
 $(document).ready(() => {
-    let baseUrl = 'http://127.0.0.1:8081'
     let agentName = '';
 
-    // Ajax
+    // Ajax 
     /* Send Transcript to Backend */
     $("#submit").click(() => {
         let conversation = $("#chat").val();
 
-        // Check for value in textarea
+        // Check for value in textarea 
         if (conversation.length < 1) return;
 
         let chatRecordId = JSON.parse(conversation).id;
 
-        if (chatRecordId) {
-            localStorage.setItem("chatRecordId", chatRecordId);
-        } else {
-            localStorage.setItem("chatRecordId", JSON.stringify(Date.now()));
-        }
-
-        loadModalContent();
-
-        let transcriptObj = formatTranscript(conversation, chatRecordId);
-
-        agentName = transcriptObj.agent_name;
-        localStorage.setItem("agentName", agentName);
-
-        $.ajax({
-            /* Write to chat_record table */
-            url: baseUrl + '/read_transcript',
-            method: 'post',
-            contentType: "application/json", // Set the request header to JSON format
-            data: JSON.stringify(transcriptObj),
-            success: function (data) {
-                if (data.code == 500) {
-                    alert(data.message)
-                } else {
-                    localStorage.setItem("feedbackRecordId", data.data)
-                    console.log('OK')
-
-                    /* Send transcript to Chat GPT */
-                    // HTTP requests across different ports on the same machine can lead to cross-domain issues.
-                    // These issues can be resolved by adding the @CrossOrigin annotation in the backend controller class.
-                    // Please refer to the backend code for specific details.
-                    // $.ajax({
-                    //     url: baseUrl + '/openai-chat',
-                    //     method: 'get',
-                    //     // data: JSON.stringify(transcriptObj),
-                    //     data: {'content': JSON.stringify(transcriptObj.transcript)},
-                    //     success: function (data) {
-                    //         if (data.code == 500) {
-                    //             alert(data.message)
-                    //         } else {
-                    //             localStorage.setItem("questions", JSON.stringify(data.data.questions))
-                    //         }
-                    //     }
-                    // })
-                }
-            }
-        })
+        sendTranscript(chatRecordId, conversation);
     })
 
-    // EventListeners
+    // EventListeners 
     /* Enable Submit(End Chat) button when there is content in textarea */
     $("#chat").change((e) => {
         let val = e.target.value;
@@ -71,40 +27,92 @@ $(document).ready(() => {
             $("#submit").prop("disabled", true);
         }
     })
-
-    // Functions
-    function formatTranscript(conversation, chatRecordId) {
-        let newConversation = [];
-        let msgArray = JSON.parse(conversation).messages;
-        let sender;
-
-        msgArray.map((item) => {
-            newConversation.push({[item.sender_name]: item.message});
-        })
-
-        // Find agent name & ignore "Talkative" sender_name if human agent was involved
-            let reverseArr = msgArray.reverse();
-            let lastAgent =     reverseArr.find((item) => {
-                return item.sender_type.includes("User");
-            })
-
-        msgArray.find((item) => {
-            if (item.sender_type.includes("User")) {
-                let agent = item.sender_name;
-                // Check if the first response and last response are from the same agent
-                agent === lastAgent.sender_name ? sender = agent : sender = lastAgent.sender_name;
-            }
-        })
-
-        let transcriptObj = {"agent_name": sender, "transcript": newConversation, "chatRecordId": chatRecordId}
-        console.log(transcriptObj)
-        return transcriptObj;
-    }
-
-    function loadModalContent() {
-        let modalContainer = new bootstrap.Modal(document.getElementById('modalContainer'))
-        $('.modal-content').load('components/startSurveyModal.html', () => {
-            modalContainer.show();
-        });
-    }
 })
+
+// Functions 
+function formatTranscript(conversation, chatRecordId) {
+    let newConversation = [];
+    let msgArray = JSON.parse(conversation).messages;
+    let sender;
+
+    msgArray.map((item) => {
+        newConversation.push({[item.sender_name]: item.message});
+    })
+
+    // Find agent name & ignore "Talkative" sender_name if human agent was involved 
+    let reverseArr = msgArray.reverse();
+    let lastAgent = reverseArr.find((item) => {
+        return item.sender_type.includes("User");
+    })
+
+    msgArray.find((item) => {
+        if (item.sender_type.includes("User")) {
+            let agent = item.sender_name;
+            // Check if the first response and last response are from the same agent 
+            agent === lastAgent.sender_name ? sender = agent : sender = lastAgent.sender_name;
+        }
+    })
+
+    let transcriptObj = {"agent_name": sender, "transcript": newConversation, "chatRecordId": chatRecordId}
+    console.log(transcriptObj)
+    return transcriptObj;
+}
+
+function loadModalContent() {
+    let modalContainer = new bootstrap.Modal(document.getElementById('modalContainer'))
+    $('.modal-content').load('components/startSurveyModal.html', () => {
+        modalContainer.show();
+    });
+}
+
+function sendTranscript(chatRecordId, conversation, widget) {
+    if (chatRecordId) {
+        localStorage.setItem("chatRecordId", chatRecordId);
+    } else {
+        localStorage.setItem("chatRecordId", JSON.stringify(Date.now()));
+    }
+
+    // Hide Talkative widget
+    document.querySelector("#talkative-engage").style.display = "none";
+
+    loadModalContent();
+
+    let transcriptObj = !widget ? formatTranscript(conversation, chatRecordId) : conversation;
+
+    agentName = transcriptObj.agent_name;
+    localStorage.setItem("agentName", agentName);
+
+    $.ajax({
+        /* Write to chat_record table */
+        url: baseUrl + '/read_transcript',
+        method: 'post',
+        contentType: "application/json", // Set the request header to JSON format
+        data: JSON.stringify(transcriptObj),
+        success: function (data) {
+            if (data.code == 500) {
+                alert(data.message)
+            } else {
+                localStorage.setItem("feedbackRecordId", data.data)
+                console.log('OK')
+
+                /* Send transcript to Chat GPT */
+                // HTTP requests across different ports on the same machine can lead to cross-domain issues.
+                // These issues can be resolved by adding the @CrossOrigin annotation in the backend controller class.
+                // Please refer to the backend code for specific details.
+                $.ajax({
+                    url: baseUrl + '/openai-chat',
+                    method: 'get',
+                    // data: JSON.stringify(transcriptObj),
+                    data: {'content': JSON.stringify(transcriptObj.transcript)},
+                    success: function (data) {
+                        if (data.code == 500) {
+                            alert(data.message)
+                        } else {
+                            localStorage.setItem("questions", JSON.stringify(data.data.questions))
+                        }
+                    }
+                })
+            }
+        }
+    })
+}
